9eb3f4e259e3a03b33b6e6c6bbc7a11c52fa66a5
Théo Créac'h <creach.t@gmail.com>
2025-02-23 12:35:42 +0100

    Ajustements Organisation atomique

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Layout from './components/organisms/Layout';
 import HomePage from './pages/HomePage';
 import JobDetailsPage from './pages/JobDetailsPage';
 import SavedJobsPage from './pages/SavedJobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="job/:id" element={<JobDetailsPage />} />
-        <Route path="saved" element={<SavedJobsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="job/:id" element={<JobDetailsPage />} />
+          <Route path="saved" element={<SavedJobsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
