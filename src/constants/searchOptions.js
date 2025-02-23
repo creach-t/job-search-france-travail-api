e0855532646336d58a37076191c34ead01282210
CREACH-T <142243266+creach-t@users.noreply.github.com>
2025-02-23 10:34:33 +0100

    feat: add constants and move options

---
+++ src/constants/searchOptions.js
@@ -0,0 +1,42 @@
+export const experienceOptions = [
+  { value: '', label: 'Toutes' },
+  { value: '1', label: 'Moins d\'un an' },
+  { value: '2', label: 'De 1 à 3 ans' },
+  { value: '3', label: 'Plus de 3 ans' }
+];
+
+export const contractOptions = [
+  { value: '', label: 'Tous' },
+  { value: 'CDI', label: 'CDI' },
+  { value: 'CDD', label: 'CDD' },
+  { value: 'MIS', label: 'Mission intérimaire' }
+];
+
+export const qualificationOptions = [
+  { value: '', label: 'Tous' },
+  { value: '0', label: 'Non-cadre' },
+  { value: '9', label: 'Cadre' }
+];
+
+export const workingHoursOptions = [
+  { value: '', label: 'Tous' },
+  { value: 'true', label: 'Temps plein' },
+  { value: 'false', label: 'Temps partiel' }
+];
+
+export const distanceOptions = [
+  { value: '0', label: 'Exactement' },
+  { value: '10', label: '10 km' },
+  { value: '20', label: '20 km' },
+  { value: '30', label: '30 km' }
+];
+
+export const webDevelopmentSkills = [
+  { value: 'javascript', label: 'JavaScript' },
+  { value: 'react', label: 'React' },
+  { value: 'angular', label: 'Angular' },
+  { value: 'vue', label: 'Vue.js' },
+  { value: 'node', label: 'Node.js' },
+  { value: 'php', label: 'PHP' },
+  { value: 'python', label: 'Python' }
+];
